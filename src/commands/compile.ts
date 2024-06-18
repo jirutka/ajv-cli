6d0cf6b1c17b1011698c33d9ef4a79c76d2e63a7
Jakub Jirutka <jakub@jirutka.cz>
2024-06-18 20:54:29 +0200

    Refactor compile.ts to simplify code

--- src/commands/compile.ts
+++ src/commands/compile.ts
@@ -1,5 +1,6 @@
-import * as fs from 'node:fs'
+import * as FS from 'node:fs'
 
+import type { Ajv } from 'ajv'
 import type { AnyValidateFunction } from 'ajv/dist/core.js'
 import standaloneCode from 'ajv/dist/standalone/index.js'
 
@@ -30,77 +31,79 @@ export default {
 
 async function compile(opts: Options, _args: string[]): Promise<boolean> {
   const ajv = await initAjv(opts, 'compile')
-  const schemaFiles = expandFilePaths(opts.schema)
-  if (schemaFiles.length > 1) {
-    return compileMultiExportModule(schemaFiles)
-  }
-  return compileSchemaAndSave(schemaFiles[0])
 
-  function compileMultiExportModule(files: string[]): boolean {
-    const validators = files.map(compileSchema)
+  const schemaPaths = expandFilePaths(opts.schema)
+
+  if (schemaPaths.length > 1) {
+    // Generate multi-export module.
+    const validators = schemaPaths.map(filepath => compileSchema(ajv, filepath))
     if (validators.every(v => v)) {
-      return saveStandaloneCode(getRefs(validators as AnyValidateFunction[], files))
+      return saveStandaloneCode(
+        ajv,
+        getRefs(validators as AnyValidateFunction[], schemaPaths),
+        opts.output,
+      )
     }
     console.error('module not saved')
-    return false
-  }
-
-  function compileSchemaAndSave(file: string): boolean {
-    const validate = compileSchema(file)
+  } else {
+    // Generate single-export module.
+    const validate = compileSchema(ajv, schemaPaths[0])
     if (validate) {
-      return saveStandaloneCode(validate)
+      return saveStandaloneCode(ajv, validate, opts.output)
     }
+  }
+  return false
+}
+
+function getRefs(validators: AnyValidateFunction[], files: string[]): { [K in string]?: string } {
+  return validators.reduce(
+    (refs, { schema }, idx) => {
+      const ref = typeof schema === 'object' ? schema.$id || files[idx] : files[idx]
+      refs[ref] = ref
+      return refs
+    },
+    {} as { [K in string]?: string },
+  )
+}
+
+function compileSchema(ajv: Ajv, schemaPath: string): AnyValidateFunction | undefined {
+  const schema = parseFile(schemaPath)
+  try {
+    const id = schema?.$id
+
+    ajv.addSchema(schema, id ? undefined : schemaPath)
+    const validate = ajv.getSchema(id || schemaPath)
+
+    console.error(`schema ${schemaPath} is valid`)
+    return validate
+  } catch (err: any) {
+    console.error(`schema ${schemaPath} is invalid`)
+    console.error(`error: ${err.message}`)
+    return
+  }
+}
+
+function saveStandaloneCode(
+  ajv: Ajv,
+  refsOrFunc: AnyValidateFunction | { [K in string]?: string },
+  output?: string,
+): boolean {
+  let moduleCode: string
+  try {
+    moduleCode = standaloneCode.default(ajv, refsOrFunc)
+  } catch (err) {
+    console.error('error preparing module:', err)
     return false
   }
-
-  function compileSchema(file: string): AnyValidateFunction | undefined {
-    const sch = parseFile(file)
-    try {
-      const id = sch?.$id
-      ajv.addSchema(sch, id ? undefined : file)
-      const validate = ajv.getSchema(id || file)
-      console.error(`schema ${file} is valid`)
-      return validate
-    } catch (err) {
-      console.error(`schema ${file} is invalid`)
-      console.error(`error: ${(err as Error).message}`)
-      return undefined
+  try {
+    if (!output) {
+      console.log(moduleCode)
+    } else {
+      FS.writeFileSync(output, moduleCode)
     }
-  }
-
-  function getRefs(
-    validators: AnyValidateFunction[],
-    files: string[],
-  ): { [K in string]?: string } {
-    return validators.reduce(
-      (refs, { schema }, idx) => {
-        const ref = typeof schema === 'object' ? schema.$id || files[idx] : files[idx]
-        refs[ref] = ref
-        return refs
-      },
-      {} as { [K in string]?: string },
-    )
-  }
-
-  function saveStandaloneCode(
-    refsOrFunc: AnyValidateFunction | { [K in string]?: string },
-  ): boolean {
-    try {
-      const moduleCode = standaloneCode.default(ajv, refsOrFunc)
-      try {
-        if (!opts.output) {
-          console.log(moduleCode)
-        } else {
-          fs.writeFileSync(opts.output, moduleCode)
-        }
-        return true
-      } catch (e) {
-        console.error('error saving file:', e)
-        return false
-      }
-    } catch (e) {
-      console.error('error preparing module:', e)
-      return false
-    }
+    return true
+  } catch (err) {
+    console.error('error saving file:', err)
+    return false
   }
 }
